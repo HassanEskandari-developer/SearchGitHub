4d806bcce43c253a1d78a27bbf75af53dea1d1b7
Meysam ghasemzadeh <meysam.meysam@gmail.com>
2024-02-07 16:14:09 +0330

    add project

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+<main>
+    <!-- https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6 -->
+
+</main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
